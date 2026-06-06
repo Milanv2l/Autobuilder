2e774fcaf68b251bcfaf67dba0717a352d13bbb0
Milanv_ <58265883+Milanv2l@users.noreply.github.com>
2026-06-06 08:23:40 +0200

    Update setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -60,4 +60,4 @@ echo -e "\n\033[92m✔ Installatie Voltooid! AutoBuilder wordt nu gestart...\033
 sleep 1
 
 # --- START AUTOBUILDER ---
-python3 "$INSTALL_DIR/autobuilder.py"
+python3 "$INSTALL_DIR/autobuilder.py" < /dev/tty
